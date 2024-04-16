5853f6508915ac3c64047248de23ddc7ae9a8e8f
xiao kun <xshalk@163.com>
2024-04-16 09:05:46 +0800

    update

--- src/main/java/com/github/shalk/ameria/tom4j/App.java
+++ src/main/java/com/github/shalk/ameria/tom4j/App.java
@@ -1,2 +1,14 @@
-package com.github.shalk.ameria.tom4j;public class App {
+package com.github.shalk.ameria.tom4j;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+  public static void main(String[] args) {
+    Path source = Paths.get("/path/to/file.toml");
+    TomlParseResult result = Toml.parse(source);
+    result.errors().forEach(error -> System.err.println(error.toString()));
+
+    String value = result.getString("a. dotted . key");
+  }
 }
