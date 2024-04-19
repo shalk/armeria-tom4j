4a54153210031ac03e448f7599402cd7e614345a
xiao kun <xshalk@163.com>
2024-04-19 09:21:19 +0800

    add more

--- src/main/java/com/github/shalk/ameria/tom4j/App.java
+++ src/main/java/com/github/shalk/ameria/tom4j/App.java
@@ -9,10 +9,15 @@ import java.util.Map;
 public class App {
   public static void main(String[] args) throws IOException, URISyntaxException {
     String path = System.getenv("HOME") + "/code/github.com/shalk/armeria/examples";
+    // find all file
     List<Path> gradleList = FindFinder.getGradleList(path);
+    // read file to memory
     List<GradleFile> gradleFiles = GradleFileManager.getGradleFile(gradleList);
+    // covert gradle file pojo to pom file pojo
     List<PomFile> pomFiles = PomFileManager.parse(gradleFiles);
+    // convert pom file pojo to pom.xml
     Map<String, String> pomFileMap = PomFileGenManager.gen(pomFiles);
+    // print it
     for (Map.Entry<String, String> entry : pomFileMap.entrySet()) {
       String value = entry.getValue();
       System.out.println("value = " + value);
