988981f1523ee2150d9b78dccafd3751c6eeefdc
shalk(xiao kun) <xshalk@163.com>
2024-05-08 11:45:54 +0800

    fix finder

--- src/main/java/com/github/shalk/armeria/tom4j/FindFinder.java
+++ src/main/java/com/github/shalk/armeria/tom4j/FindFinder.java
@@ -21,7 +21,7 @@ public class FindFinder {
             new BiPredicate<Path, BasicFileAttributes>() {
               @Override
               public boolean test(Path path, BasicFileAttributes basicFileAttributes) {
-                return path.endsWith("build.gradle") && !path.getParent().endsWith("-scala") && !path.getParent().endsWith("-sangria");
+                return path.endsWith("build.gradle") && !path.getParent().toString().endsWith("-scala") && !path.getParent().toString().endsWith("-sangria");
               }
             });
     return pathStream.collect(Collectors.toList());
